e054d5af07ed772a4627a8a53df2b29db739a672
aroralakshita <alakshita07@gmail.com>
2025-09-14 00:57:21 +0200

    orgprofile only if logged in

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
 
-function ProtectedRoute({ user, children }) {
-  if (!user) {
-    return <Navigate to="/login" replace />;
+export default function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+  const toast = useToast();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!token) {
+      toast({
+        title: "Access denied",
+        description: "You must be logged in to view this page.",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  }, [token, toast]);
+
+  if (!token) {
+    return <Navigate to="/orgmap" state={{ from: location }} replace />;
   }
 
   return children;
 }
-
-export default ProtectedRoute;
