3801db2ab3728d20abead47c4af673d04e8fa621
aroralakshita <alakshita07@gmail.com>
2026-01-16 01:32:04 +0100

    changed font family

--- client/src/theme.js
+++ client/src/theme.js
@@ -13,14 +13,17 @@ const colors = {
 const theme = extendTheme({
   colors,
   fonts: {
-    heading: "'Ubuntu', sans-serif",
-    body: "'Ubuntu', sans-serif",
+    heading: "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
+    body: "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
   },
   styles: {
     global: {
       body: {
         bg: "brand.50",
         color: "gray.800",
+
+        WebkitFontSmoothing: "antialiased",
+        MozOsxFontSmoothing: "grayscale",
       },
     },
   },
